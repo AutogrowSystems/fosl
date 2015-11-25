816180b66ac310028900725f2e0b1efc69346178
Robert McLeod <robert@penguinpower.co.nz>
2015-11-25 16:26:10 +1300

    Added some nicer looking commands
    Now you can do require 'fosl' which creates the method lsof() in the global namespace.  Which is just a shortcut to FOSL.lsof()

---
+++ lib/fosl.rb
@@ -0,0 +1,16 @@
+require 'fosl/parser'
+require 'fosl/process'
+
+module FOSL
+  class << self
+    def lsof(args)
+      FOSL::Parser.new.lsof(args)
+    end
+  end
+end
+
+unless defined?(:lsof)
+  def lsof(args)
+    FOSL.lsof(args)
+  end
+end
